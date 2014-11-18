151968dfe3754aa40ae8fb93dbf569826c0551e3
Steve Hay <steve.m.hay@googlemail.com>
2014-11-18 19:56:22 +0000

    Minor formatting changes

--- CryptFile/lib/Filter/Crypto/CryptFile.pm
+++ CryptFile/lib/Filter/Crypto/CryptFile.pm
@@ -88,8 +88,8 @@ sub AUTOLOAD {
 
     # Generate an in-line subroutine returning the required value.
     {
-        no strict 'refs'; ## no critic (TestingAndDebugging::ProhibitNoStrict)
-        *$AUTOLOAD = sub { return $value };
+    no strict 'refs'; ## no critic (TestingAndDebugging::ProhibitNoStrict)
+    *$AUTOLOAD = sub { return $value };
     }
 
     # Switch to the subroutine that we have just generated.
