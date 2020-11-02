c8e739a42d276a652e4e3b92a9db4af6599b43d4
Steve Hay <steve.m.hay@googlemail.com>
2020-11-02 08:34:56 +0000

    Use https instead of http where possible

--- lib/Filter/Crypto.pm
+++ lib/Filter/Crypto.pm
@@ -116,7 +116,7 @@ decryption filter.
 
 Some of the points below come from a discussion on the perl5-porters mailing
 list, in the thread starting here:
-L<http://www.xray.mpe.mpg.de/mailing-lists/perl5-porters/2003-10/msg01169.html>;
+L<https://www.nntp.perl.org/group/perl.perl5.porters/2003/10/msg84051.html>;
 others are taken from the L<Filter::decrypt|Filter::decrypt> manpage.
 
 In general, it is hopeless to try to prevent everyone from getting at the source
@@ -253,13 +253,13 @@ Currently active requests on the CPAN Request Tracker can be viewed at
 L<https://rt.cpan.org/Public/Dist/Display.html?Status=Active;Queue=Filter-Crypto>.
 
 Please test this distribution.  See CPAN Testers Reports at
-L<http://www.cpantesters.org/> for details of how to get involved.
+L<https://www.cpantesters.org/> for details of how to get involved.
 
 Previous test results on CPAN Testers Reports can be viewed at
-L<http://www.cpantesters.org/distro/F/Filter-Crypto.html>.
+L<https://www.cpantesters.org/distro/F/Filter-Crypto.html>.
 
 Please rate this distribution on CPAN Ratings at
-L<http://cpanratings.perl.org/rate/?distribution=Filter-Crypto>.
+L<https://cpanratings.perl.org/rate/?distribution=Filter-Crypto>.
 
 =head1 SEE ALSO
 
@@ -286,9 +286,9 @@ L<perlmodlib/"CPAN"> for details) at
 
 L<https://metacpan.org/release/Filter-Crypto> or
 
-L<http://www.cpan.org/authors/id/S/SH/SHAY/> or
+L<https://www.cpan.org/authors/id/S/SH/SHAY/> or
 
-L<http://www.cpan.org/modules/by-module/Filter/>.
+L<https://www.cpan.org/modules/by-module/Filter/>.
 
 The latest source code is available from GitHub at
 L<https://github.com/steve-m-hay/Filter-Crypto>.
